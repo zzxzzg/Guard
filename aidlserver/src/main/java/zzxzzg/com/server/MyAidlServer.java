05ab4920bebfc86b40a7acfe8f906dbc6f28107d
王彦骁 <xiaobanma2292@gmail.com>
2016-04-10 17:09:02 +0800

    fix a aidl server callback bug

--- aidlserver/src/main/java/zzxzzg/com/server/MyAidlServer.java
+++ aidlserver/src/main/java/zzxzzg/com/server/MyAidlServer.java
@@ -3,12 +3,16 @@ package zzxzzg.com.server;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteCallbackList;
 import android.os.RemoteException;
 
 /**
  * Created by apple on 16/4/9.
  */
 public class MyAidlServer extends Service{
+
+    private RemoteCallbackList<ITaskCallback> mCallbacks=new RemoteCallbackList<>();
+
     @Override
     public IBinder onBind(Intent intent) {
         return new MyAidlServerImpl();
@@ -23,17 +27,29 @@ public class MyAidlServer extends Service{
 
         @Override
         public void registerCallback(ITaskCallback callback) throws RemoteException {
-
+            mCallbacks.register(callback);
         }
 
         @Override
         public void unregisterCallback(ITaskCallback callback) throws RemoteException {
-
+            mCallbacks.unregister(callback);
         }
 
         @Override
         public IBinder asBinder() {
             return null;
         }
+    }
+
+    public void callback(){
+        int count=mCallbacks.beginBroadcast();
+        try {
+        for(int i=0;i<count;i++){
+                mCallbacks.getBroadcastItem(i).actionPerformed(1);
+        }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        mCallbacks.finishBroadcast();
     }
 }
